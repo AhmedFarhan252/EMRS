210c95934450a21adb4afa0d60463e0aa29ba9c8
Ahmed Farhan <ahmedfarhan252@gmail.com>
2020-04-22 01:59:11 +0500

    Added adminController

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,15 +1,83 @@
 const db = require("../db/dbConnector");
 
-db.any(
-  `
-  select * from doctor;
-`
-)
-  .then((data) => {
-    data.forEach((r) => {
-      console.log(r);
+exports.getAccount = function (req, res) {
+  db.any(
+    "select doctor.f_name,doctor.l_name,doctor.email from doctor,login where doctor.email = login.email and login.user_role = 'doctor';"
+  )
+    .then((data) => {
+      res.json({
+        status: "ok",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
     });
+};
+
+exports.newAccount = function (req, res) {
+  const { fname, lname, email, num, cnic } = req.body;
+  console.log(req.body);
+  db.task((t) => {
+    return t
+      .none(
+        "insert into doctor (cnic,f_name,l_name,email,phone_num) values ($1,$2,$3,$4,$5);",
+        [cnic, fname, lname, email, num]
+      )
+      .then(() => {
+        return t.none(
+          "insert into login (email,user_role) values ($1,'doctor');",
+          [email]
+        );
+      });
   })
-  .catch((err) => {
-    console.log(err);
-  });
+    .then(() => {
+      res.json({
+        status: "ok",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.delAccount = function (req, res) {
+  const email = req.body.email;
+  db.none("delete from login where email = $1 and user_role = 'doctor';", [
+    email,
+  ])
+    .then(() => {
+      res.json({
+        status: "ok",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.getDisease = function (req, res) {
+  db.any("SELECT enum_range(NULL::disease);")
+    .then((data) => {
+      res.json({
+        status: "ok",
+        data: data,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+exports.addDisease = function (req, res) {
+  const dis_name = req.body.disease;
+  db.none("alter type disease add value $1 ;", [dis_name])
+    .then(() => {
+      res.json({
+        status: "ok",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 // admin.js - admin route module
 const express = require("express");
 const router = express.Router();
+const admin_controller = require("../controllers/adminController");
 
 // Home page route
 router.get("/", (req, res) => {
@@ -8,28 +9,28 @@ router.get("/", (req, res) => {
 });
 
 // Account management route
-router.get("/accounts/:id", (req, res) => {
-  res.send("Accounts menu");
+router.get("/accounts", (req, res) => {
+  admin_controller.getAccount(req, res);
 });
 
 // new account route
 router.post("/accounts", (req, res) => {
-  res.send("Create new account");
+  admin_controller.newAccount(req, res);
 });
 
 // delete account route
 router.post("/delaccount", (req, res) => {
-  res.send("Delete selected account");
+  admin_controller.delAccount(req, res);
 });
 
 // Disease route
-router.get("/disease/:id", (req, res) => {
-  res.send("Disease menu");
+router.get("/disease", (req, res) => {
+  admin_controller.getDisease(req, res);
 });
 
 // Disease add route
 router.post("/disease", (req, res) => {
-  res.send("Add Disease ");
+  admin_controller.addDisease(req, res);
 });
 
 module.exports = router;
