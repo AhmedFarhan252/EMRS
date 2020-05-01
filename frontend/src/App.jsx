20c66bf0857fc8647a1b7ff51f6f03506fcf1d8f
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2020-05-02 00:05:52 +0500

    Add files via upload
    added route for records

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,40 @@
-import React from "react";
-// import { Route, NavLink, Link } from "react-router-dom";
-// import { BrowserRouter as Router } from "react-router-dom";
-import Profile from "./components/profile.jsx";
-import NavBar from "./components/navbar.jsx";
-import UpperBar from "./components/upper-bar.jsx";
-import "./css/App.css";
+import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route } from "react-router-dom"
+import Profile from "./components/profile.jsx"
+import NavBar from "./components/navbar.jsx"
+import UpperBar from "./components/upper-bar.jsx"
+import Edit from "./components/edit-profile.jsx"
+import Register from "./components/registration"
+import Records from "./components/records.jsx"
+import "./css/App.css"
 
 class App extends React.Component {
-  render() {
-    return (
-      <div className={"row no-gutters"}>
-        <div className={"col-sm-2 no-gutters"}>
-          <div className={"leftside"}>
-            <NavBar />
-          </div>
-        </div>
-        <div className={"col-sm-10 no-gutters"}>
-          <div className={"rightside"}>
-            <UpperBar />
-            <Profile />
-          </div>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className={"row no-gutters"}>
+					<div className={"col-sm-2 no-gutters"}>
+						<div className={"leftside"}>
+							<NavBar />
+						</div>
+					</div>
+					<div className={"col-sm-10 no-gutters"}>
+						<div
+							className={"rightside"}
+							style={{ backgroundColor: "#eceeef" }}
+						>
+							<UpperBar />
+							<Register />
+						</div>
+					</div>
+					<Route path="/profile" component={Profile} />
+					<Route path="/edit-profile" component={Edit} />
+					<Route path="/records" component={Records} />
+				</div>
+			</Router>
+		)
+	}
 }
 
-export default App;
+export default App
