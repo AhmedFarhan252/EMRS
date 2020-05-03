61bc05fab6845ce498bba9feef50b62a0fd83910
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2020-05-03 11:40:34 +0500

    Add files via upload
    made the design more responsive

--- frontend/src/components/registration.jsx
+++ frontend/src/components/registration.jsx
@@ -4,161 +4,205 @@ import logo from "../img/LogoEmr.png"
 import "../css/registration.css"
 
 class Register extends Component {
-	state = { cnic: null, dob: null, contact: null, sex: null, blood: null }
+	constructor() {
+		super()
+		this.state = {
+			fname: "",
+			lname: "",
+			cnic: "",
+			dob: "",
+			contact: "",
+			gender: "",
+			blood: "",
+		}
+	}
+
+	ChangeHandle = event => {
+		this.setState({ [event.target.name]: event.target.value })
+	}
+
 	render() {
 		const MainContainer = {
-			height: "90vh",
+			height: "100vh",
 			backgroundImage:
 				"linear-gradient(198deg, rgba(9,9,121,1) 0%, rgba(0,212,255,1) 100%)",
 		}
 		const TitleStyle = {
 			fontWeight: 700,
-			margin: 0,
-			marginLeft: 370,
+			fontSize: "2.5em",
+			margin: "auto",
 			color: "#d2202f",
 		}
 		const HeaderStyle = {
 			backgroundColor: "#eceeef",
-			width: "140.6vh",
-			height: "8vh",
-			marginLeft: 90,
+			width: "90%",
+			height: "5%",
+			margin: "auto",
 			borderTop: "5px solid #d2202f",
 			borderTopLeftRadius: "30px",
 			borderTopRightRadius: "30px",
+			display: "flex",
+			justifyContent: "center",
 		}
-		const FormStyle = {
+		const inputForm = {
+			width: "90%",
+			height: "80%",
+			margin: "auto",
 			backgroundColor: "#eceeef",
-			height: "67vh",
 			borderBottom: "5px solid #d2202f",
 			borderBottomLeftRadius: "30px",
 			borderBottomRightRadius: "30px",
 		}
 		const InputStyle = {
-			border: "none",
 			width: "100%",
-			height: "6.5vh",
+			height: "6.7vh",
 			marginTop: 7,
 			border: "1.5px solid #2d8fd5",
 			borderRadius: "10px",
-			textIndent: "10px",
+			textIndent: "1%",
 		}
 		const LabelStyle = {
 			color: "#006fbe",
-			fontSize: "17px",
-			marginTop: 25,
-			marginBottom: 0,
+			fontSize: "1em",
+			marginTop: "3%",
+			marginBottom: "0%",
 		}
 		const ImgStyle = {
-			padding: "14px 24px",
-			marginLeft: 460,
+			padding: "1% 2%",
+			margin: "auto",
+			width: "10%",
+			height: "10%",
 		}
 		return (
 			<div className="container-fluid" style={MainContainer}>
-				<div
-					className="row jutify-content-center"
-					style={{ height: "10vh" }}
-				></div>
+				<div className="row" style={{ height: "10%" }}></div>
 				<header style={HeaderStyle}>
 					<img src={logo} alt="EMR" style={ImgStyle} />
 				</header>
-				<div className="row justify-content-center">
-					<div className="col-10" style={FormStyle}>
-						<h1 style={TitleStyle}>Create Account</h1>
-						<form>
-							<div
-								className="col-11 justify-content-center"
-								style={{ marginLeft: 55 }}
-							>
-								<label htmlFor="cnic" style={LabelStyle}>
-									CNIC
+				<div className="row justify-content-center" style={inputForm}>
+					<h1 style={TitleStyle}>Create Account</h1>
+					<form>
+						<div
+							className="col-11 justify-content-center"
+							style={{ margin: "auto" }}
+						>
+							<label htmlFor="cnic" style={LabelStyle}>
+								CNIC
+							</label>
+							<input
+								type="number"
+								name="cnic"
+								value={this.state.value}
+								style={InputStyle}
+								placeholder="CNIC NUMBER"
+								onChange={this.ChangeHandle}
+							/>
+						</div>
+						<div
+							className="row justify-content-around "
+							style={{ margin: "0% -5%" }}
+						>
+							<div className="col-4">
+								<label htmlFor="fname" style={LabelStyle}>
+									First Name
 								</label>
 								<input
 									type="text"
-									id="cnic"
+									name="fname"
+									value={this.state.fname}
 									style={InputStyle}
-									placeholder="CNIC NUMBER"
+									placeholder="First Name"
 									onChange={this.ChangeHandle}
 								/>
-							</div>
-							<div className="row">
-								<div
-									className="col-4 justify-content-center"
-									style={{ marginLeft: 70 }}
+								<label htmlFor="dob" style={LabelStyle}>
+									DOB
+								</label>
+								<input
+									type="date"
+									name="dob"
+									style={InputStyle}
+									value={this.state.dob}
+									placeholder="DD/MM/YYYY"
+									onChange={this.ChangeHandle}
+								/>
+								<label htmlFor="gender" style={LabelStyle}>
+									Gender
+								</label>
+								<select
+									className="ArrowStyle"
+									name="gender"
+									value={this.state.gender}
+									style={InputStyle}
+									placeholder="Select Your Gender"
+									onChange={this.ChangeHandle}
 								>
-									<label htmlFor="dob" style={LabelStyle}>
-										DOB
-									</label>
-									<input
-										type="date"
-										id="dob"
-										style={InputStyle}
-										placeholder="DD/MM/YYYY"
-										onChange={this.ChangeHandle}
-									/>
-									<label htmlFor="sex" style={LabelStyle}>
-										Sex
-									</label>
-									<input
-										type="text"
-										id="sex"
-										style={InputStyle}
-										placeholder="Select Your Gender"
-										onChange={this.ChangeHandle}
-									/>
-									<NavLink to="/profile">
-										<input
-											type="submit"
-											value="Save"
-											onSubmit={this.SubmissionHandle}
-											className="BtnStyle"
-										></input>
-									</NavLink>
-								</div>
-								<div
-									className="col-4 justify-content-center"
-									style={{ marginLeft: 233 }}
-								>
-									<label htmlFor="contact" style={LabelStyle}>
-										Contact No.
-									</label>
-									<input
-										type="text"
-										id="contact"
-										style={InputStyle}
-										placeholder="Phone Number"
-										onChange={this.ChangeHandle}
-									/>
-									<label htmlFor="blood" style={LabelStyle}>
-										Blood Group
-									</label>
-									<select
-										className="ArrowStyle"
-										id="blood"
-										style={InputStyle}
-										placeholder="Select Your Blood Group"
-										onChange={this.ChangeHandle}
-									>
-										<option value="A+">A+</option>
-										<option value="A-">A-</option>
-										<option value="B+">B+</option>
-										<option value="B-">B-</option>
-										<option value="AB+">AB+</option>
-										<option value="AB-">AB-</option>
-										<option value="O+">O+</option>
-										<option value="O-">O-</option>
-									</select>
-									<NavLink to="/profile">
-										<input
-											type="button"
-											value="Cancel"
-											onSubmit={this.SubmissionHandle}
-											className="BtnStyle"
-										/>
-									</NavLink>
-								</div>
+									<option value="Male">Male</option>
+									<option value="Female">Female</option>
+									<option value="Other">Other</option>
+								</select>
+								<input
+									type="submit"
+									value="Create"
+									onSubmit={this.SubmissionHandle}
+									className={"ButtonStyle"}
+									onChange={this.ChangeHandle}
+								></input>
 							</div>
-						</form>
-					</div>
+
+							<div className="col-4 ">
+								<label htmlFor="lname" style={LabelStyle}>
+									Last Name
+								</label>
+								<input
+									type="text"
+									name="lname"
+									value={this.state.lname}
+									style={InputStyle}
+									placeholder="Last Name"
+									onChange={this.ChangeHandle}
+								/>
+								<label htmlFor="contact" style={LabelStyle}>
+									Contact No.
+								</label>
+								<input
+									type="number"
+									name="contact"
+									value={this.state.contact}
+									style={InputStyle}
+									placeholder="Phone Number"
+									onChange={this.ChangeHandle}
+								/>
+								<label htmlFor="blood" style={LabelStyle}>
+									Blood Group
+								</label>
+								<select
+									className="ArrowStyle"
+									name="blood"
+									value={this.state.blood}
+									style={InputStyle}
+									placeholder="Select Your Blood Group"
+									onChange={this.ChangeHandle}
+								>
+									<option value="A+">A+</option>
+									<option value="A-">A-</option>
+									<option value="B+">B+</option>
+									<option value="B-">B-</option>
+									<option value="AB+">AB+</option>
+									<option value="AB-">AB-</option>
+									<option value="O+">O+</option>
+									<option value="O-">O-</option>
+								</select>
+								<NavLink to="/">
+									<input
+										type="button"
+										value="Cancel"
+										onSubmit={this.SubmissionHandle}
+										className="ButtonStyle"
+									/>
+								</NavLink>
+							</div>
+						</div>
+					</form>
 				</div>
 			</div>
 		)
