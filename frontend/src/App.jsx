cbd0478b76495cc75a150109fab215d60253dcaa
nan-af <afnanmughal@hotmail.com>
2020-05-04 02:29:13 +0500

    Merge changes from client branch

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from "react";
 // import { Route, NavLink, Link } from "react-router-dom";
 // import { BrowserRouter as Router } from "react-router-dom";
@@ -12,6 +11,7 @@ import Register from "./components/registration";
 import Records from "./components/records";
 import DocAccounts from "./components/doc-accounts";
 import AddDocAccount from "./components/add-doc-account";
+import View from "./components/view-record";
 import "./css/App.css";
 
 class App extends React.Component {
@@ -25,7 +25,10 @@ class App extends React.Component {
             </div>
           </div>
           <div className={"col-sm-10 no-gutters"}>
-            <div className={"rightside"}>
+            <div
+              className={"rightside"}
+              style={{ backgroundColor: "#eceeef" }}
+            >
               <UpperBar />
 
               <Switch>
@@ -36,6 +39,7 @@ class App extends React.Component {
                 <Route path="/records" component={Records} />
                 <Route path="/doc-accounts" component={DocAccounts} />
                 <Route path="/add-doc-account" component={AddDocAccount} />
+                <Route path="/view-record" component={View} />
               </Switch>
             </div>
           </div>
@@ -43,46 +47,6 @@ class App extends React.Component {
       </BrowserRouter >
     );
   }
-=======
-import React from "react"
-import { BrowserRouter as Router } from "react-router-dom"
-import { Route } from "react-router-dom"
-import Profile from "./components/profile.jsx"
-import NavBar from "./components/navbar.jsx"
-import UpperBar from "./components/upper-bar.jsx"
-import Edit from "./components/edit-profile.jsx"
-import Register from "./components/registration"
-import Records from "./components/records.jsx"
-import View from "./components/view-record"
-import "./css/App.css"
-
-class App extends React.Component {
-	render() {
-		return (
-			<Router>
-				<div className={"row no-gutters"}>
-					<div className={"col-sm-2 no-gutters"}>
-						<div className={"leftside"}>
-							<NavBar />
-						</div>
-					</div>
-					<div className={"col-sm-10 no-gutters"}>
-						<div
-							className={"rightside"}
-							style={{ backgroundColor: "#eceeef" }}
-						>
-							<UpperBar />
-							<View />
-						</div>
-					</div>
-					<Route path="/profile" component={Profile} />
-					<Route path="/edit-profile" component={Edit} />
-					<Route path="/records" component={Records} />
-				</div>
-			</Router>
-		)
-	}
->>>>>>> origin/client
 }
 
 export default App
