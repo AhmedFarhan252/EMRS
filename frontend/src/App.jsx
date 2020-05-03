b7b76d64117688531049addccec58218f06e4bf5
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2020-05-03 20:36:27 +0500

    Routes established

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
 import React from "react"
 import { BrowserRouter as Router } from "react-router-dom"
 import { Route } from "react-router-dom"
-import Profile from "./components/profile.jsx"
+import Profile from "./components/Patient/profile.jsx"
 import NavBar from "./components/navbar.jsx"
 import UpperBar from "./components/upper-bar.jsx"
-import Edit from "./components/edit-profile.jsx"
-import Register from "./components/registration"
-import Records from "./components/records.jsx"
-import View from "./components/view-record"
-import "./css/App.css"
+import Edit from "./components/Patient/edit-profile.jsx"
+import Register from "./components/registration.jsx"
+import Records from "./components/Patient/records.jsx"
+import View from "./components/Patient/view-record.jsx"
+import Login from "./components/login.jsx"
+import Accounts from "./components/Admin/accounts.jsx"
+import DocProfile from "./components/Dr/profile.jsx"
+import DocEdit from "./components/Dr/edit-profile.jsx"
+import "./App.css"
 
 class App extends React.Component {
 	render() {
@@ -26,16 +30,46 @@ class App extends React.Component {
 							style={{ backgroundColor: "#eceeef" }}
 						>
 							<UpperBar />
-							<View />
+							<DocProfile />
 						</div>
 					</div>
-					<Route path="/profile" component={Profile} />
-					<Route path="/edit-profile" component={Edit} />
-					<Route path="/records" component={Records} />
 				</div>
+				<Route path="/profile" component={Profile} />
+				<Route path="/edit-profile" component={Edit} />
+				<Route path="/records" component={Records} />
+				<Route path="/view-record" component={View} />
+				
 			</Router>
 		)
 	}
 }
 
 export default App
+
+/*import React from "react"
+import { BrowserRouter as Router } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
+import Register from "./components/registration"
+import Login from "./components/login"
+import Patient from "./components/Routes/patient"
+import Doctor from "./components/Routes/doctor"
+import Admin from "./components/Routes/admin"
+import "./App.css"
+
+class App extends React.Component {
+	render() {
+		return (
+			<Router>
+				<Switch>
+					<Route exact path="/" component={Login} />
+					<Route path="/newprofile" component={Register} />
+					<Route path="/Patient" component={Patient} />
+					<Route path="/Dr" component={Doctor} />
+					<Route path="/Admin" component={Admin} />
+				</Switch>
+			</Router>
+		)
+	}
+}
+
+export default App*/
