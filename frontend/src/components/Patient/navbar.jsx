e566530d7622aaf528ea8a9a06baeb75e4badca0
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2020-05-04 20:03:48 +0500

    updated routes

--- frontend/src/components/Patient/navbar.jsx
+++ frontend/src/components/Patient/navbar.jsx
@@ -1,22 +1,22 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
-import "../css/navbar.css";
-import logo from "../../img/LogoEmr.png";
+import React from "react"
+import { NavLink } from "react-router-dom"
+import "../css/navbar.css"
+import logo from "../../img/LogoEmr.png"
 
 const NavBar = () => (
-  <div className="sidenav">
-    <div className={"logostyle"}>
-      <img src={logo} alt="EMR" />
-    </div>
-    <ul>
-      <li>
-        <NavLink to="/patient/profile">Profile</NavLink>
-      </li>
-      <li>
-        <NavLink to="/patient/records">Records</NavLink>
-      </li>
-    </ul>
-  </div>
-);
+	<div className="sidenav">
+		<div className={"logostyle"}>
+			<img src={logo} alt="EMR" />
+		</div>
+		<ul>
+			<li>
+				<NavLink to="/p/profile">Profile</NavLink>
+			</li>
+			<li>
+				<NavLink to="/p/records">Records</NavLink>
+			</li>
+		</ul>
+	</div>
+)
 
-export default NavBar;
+export default NavBar
