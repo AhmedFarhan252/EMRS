d4ecf224522810a35bf673b34d0ef8181e21e255
Ammar Waheed <33984786+BlazingC0der@users.noreply.github.com>
2020-05-04 13:26:37 +0500

    updated routes

--- frontend/src/components/Dr/profile.jsx
+++ frontend/src/components/Dr/profile.jsx
@@ -106,7 +106,7 @@ class DocProfile extends Component {
 					<div className="col">CONTACT NO.</div>
 				</div>
 				<div className="row" style={{ padding: "45px 35px" }}></div>
-				<NavLink to="/doc-edit-profile">
+				<NavLink to="/Dr/edit-profile">
 					<button
 						type="button"
 						className="btn btn-outline-primary"
